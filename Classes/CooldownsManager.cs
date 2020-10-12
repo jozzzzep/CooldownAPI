8321bc65fd77b77f12f5c41d221c465f2fb76e11
JosepeDev <yosefd99@gmail.com>
2020-10-12 15:14:46 +0300

    Updated comments

--- Classes/CooldownsManager.cs
+++ Classes/CooldownsManager.cs
@@ -2,37 +2,37 @@ using System;
 
 public class CooldownsManager
     {
-        // A class for handeling and managing cooldowns efficiently
-        //
-        // ---- INFO AND TOOLS:
-        // 
-        // DecreaseCooldowns() - Call this method inside Update() to decrease all the cooldowns
-        // NewCooldown() - Creates and returns a new cooldown, just input the cooldown's duration.
-        //
-        // ---- TUTORIAL(For beginners):
-        //
-        // 1.
-        // For managing cooldowns in a class create and initialize a new CooldownManager.
-        // Example: CooldownManager cooldownManager = new CooldownManager();
-        //
-        // 2.
-        // To create a new cooldown create the variable in the class.
-        // Example: Cooldown nameOfYourCooldown;
-        // 
-        // 3.
-        // Inside Start() or Awake() assign each cooldown to the method NewCooldown() and input the cooldown's duration.
-        // Example: nameOfYourCooldown = CooldownManager.NewCooldown(4f);
-        //
-        // 4.
-        // Final step, call the decreaseCooldowns Action in the Update() method.
-        // Example: decreaseCooldowns();
-        //
-        // ----
+    // A class for handeling and managing cooldowns efficiently
+    //
+    // ---- INFO AND TOOLS:
+    // 
+    // DecreaseCooldowns() - Call this method inside Update() to decrease all the cooldowns
+    // NewCooldown() - Creates and returns a new cooldown, just input the cooldown's duration.
+    //
+    // ---- TUTORIAL(For beginners):
+    //
+    // 1.
+    // For managing cooldowns in a class create and initialize a new CooldownManager.
+    // Example: CooldownManager cooldownManager = new CooldownManager();
+    //
+    // 2.
+    // To create a new cooldown create the variable in the class.
+    // Example: Cooldown nameOfYourCooldown;
+    // 
+    // 3.
+    // Inside Start() or Awake() assign each cooldown to the method NewCooldown() and input the cooldown's duration.
+    // Example: nameOfYourCooldown = cooldownManager.NewCooldown(4f);
+    //
+    // 4.
+    // Final step, call the method DecreaseCooldowns() in the Update() method.
+    // Example: cooldownManager.DecreaseCooldowns();
+    //
+    // ----
 
-        #region Variables
+    #region Variables
 
-        // when called it'll decrease every cooldown you created with this object
-        private Action decreaseCooldowns;
+    // when called it'll decrease every cooldown you created with this object
+    private Action decreaseCooldowns;
 
         #endregion
 
