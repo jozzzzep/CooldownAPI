a1640e13d7bbc8be0eba9f1b049408418028ecfe
JosepeDev <yosefd99@gmail.com>
2020-10-12 16:39:59 +0300

    Fixed comments

--- Classes/CooldownsManager.cs
+++ Classes/CooldownsManager.cs
@@ -1,75 +1,58 @@
 using System;
 
-public class CooldownsManager
-    {
-    // A class for handeling and managing cooldowns efficiently
+public class CooldownsManager 
+{
+    // A class for handling and managing multiple cooldowns efficiently
     //
-    // ---- INFO AND TOOLS:
+    // Wiki page: https://github.com/JosepeDev/UnityCooldownsHandler/wiki/CooldownsManager
+    // Examples and tutorial: https://github.com/JosepeDev/UnityCooldownsHandler/wiki/Examples-&-Tutorial
+    //
+    // INFO AND TOOLS:
     // 
     // DecreaseCooldowns() - Call this method inside Update() to decrease all the cooldowns
     // NewCooldown() - Creates and returns a new cooldown, just input the cooldown's duration.
-    //
-    // ---- TUTORIAL(For beginners):
-    //
-    // 1.
-    // For managing cooldowns in a class create and initialize a new CooldownManager.
-    // Example: CooldownManager cooldownManager = new CooldownManager();
-    //
-    // 2.
-    // To create a new cooldown create the variable in the class.
-    // Example: Cooldown nameOfYourCooldown;
-    // 
-    // 3.
-    // Inside Start() or Awake() assign each cooldown to the method NewCooldown() and input the cooldown's duration.
-    // Example: nameOfYourCooldown = cooldownManager.NewCooldown(4f);
-    //
-    // 4.
-    // Final step, call the method DecreaseCooldowns() in the Update() method.
-    // Example: cooldownManager.DecreaseCooldowns();
-    //
-    // ----
 
     #region Variables
 
     // when called it'll decrease every cooldown you created with this object
     private Action decreaseCooldowns;
 
-        #endregion
+    #endregion
 
-        #region Methods
+    #region Methods
 
-        public Cooldown NewCooldown(float duration)
-        {
-            // create a new cooldown
-            Cooldown returnThisCooldown = new Cooldown(duration);
+    public Cooldown NewCooldown(float duration)
+    {
+        // create a new cooldown
+        Cooldown returnThisCooldown = new Cooldown(duration);
 
-            // add to Action delegate
-            AssignCooldownToDelegate(returnThisCooldown);
+        // add to Action delegate
+        AssignCooldownToDelegate(returnThisCooldown);
 
-            // return the new cooldown
-            return returnThisCooldown;
-        }
-
-        // calls the delegate from outside the class
-        public void DecreaseCooldowns()
-        {
-            decreaseCooldowns();
-        }
-        
-        private void AssignCooldownToDelegate(Cooldown cooldown)
-        {
-            // delegate has no subscribers
-            if(decreaseCooldowns == null)
-            {
-                // assign the first subscriber
-                decreaseCooldowns = cooldown.DecreaseCooldown;
-            }
-            else
-            {
-                // add an additional subscriber
-                decreaseCooldowns += cooldown.DecreaseCooldown;
-            }
-        }
-
-        #endregion
+        // return the new cooldown
+        return returnThisCooldown;
     }
+
+    // calls the delegate from outside the class
+    public void DecreaseCooldowns()
+    {
+        decreaseCooldowns();
+    }
+
+    private void AssignCooldownToDelegate(Cooldown cooldown)
+    {
+        // delegate has no subscribers
+        if (decreaseCooldowns == null)
+        {
+            // assign the first subscriber
+            decreaseCooldowns = cooldown.DecreaseCooldown;
+        }
+        else
+        {
+            // add an additional subscriber
+            decreaseCooldowns += cooldown.DecreaseCooldown;
+        }
+    }
+
+    #endregion
+}
