13ac5849b5e009882fe8e4b9aa0a93eb2b768d46
JosepeDev <yosefd99@gmail.com>
2021-06-08 17:47:50 +0300

    Update v0.3
    - Made it even simpler and easier to use the API
      - Now you don't even need to use the cooldown manager manually. It will be called and initialized automatically when you need it in the background.
      - All you need to do now to create a cooldown is to simply call the default constructor.
    - Added a property to get the value from the timer of the cooldown.
    - Now the scripts are inside a separate namespace called CooldownAPI
    - Fixed a bug that caused the timer to get bellow zero
    - Fixed documentation mistakes and reorganized the code

--- CooldownsManager.cs
+++ CooldownsManager.cs
@@ -1,64 +1,34 @@
 using System;
+using UnityEngine;
 
-public class CooldownsManager 
+namespace CooldownAPI
 {
-    /// A class for handling and managing multiple cooldowns efficiently.
-    ///
-    /// WIKI & INFO: https://github.com/JosepeDev/CooldownAPI
-    ///
-    /// Methods:
-    /// - Update()      - IMPORTANT Call this method on Update() inside a MonoBehaviour to Update all the cooldowns
-    /// - NewCooldown() - Creates and returns a new cooldown, just input the cooldown's duration.
-
-    #region Variables
-
-    // when called it'll decrease every cooldown you created with this object
-    private Action cooldownsUpdateMethods;
-
-    #endregion
-
-    #region Methods
-
-    /// <summary>
-    /// Returns a new <see cref="Cooldown"/> object and subscribes it to the <see cref="CooldownsManager"/> it has been created with.
-    /// <para> When you call the <see cref="Update()"/>, it updates all the cooldowns you created with <see cref="NewCooldown(float)"/> at once </para>
-    /// </summary>
-    /// <param name="duration"> The default duration you want the <see cref="Cooldown"/> object to have.</param>
-    /// <returns></returns>
-    public Cooldown NewCooldown(float duration)
+    internal class CooldownsManager : MonoBehaviour
     {
-        // create a new cooldown
-        Cooldown returnThisCooldown = new Cooldown(duration);
+        /// The class that manages and calls all the cooldowns
+        /// No need to use or call it manually 
+        /// Tutorial and Examples: https://github.com/JosepeDev/CooldownAPI
 
-        // add to Action delegate
-        AssignCooldownToDelegate(returnThisCooldown);
+        private Action cooldownsUpdates;
 
-        // return the new cooldown
-        return returnThisCooldown;
-    }
-
-    /// <summary>
-    /// IMPORTANT >> Call this method on Update() inside a MonoBehaviour to Update all the cooldowns
-    /// </summary>
-    public void Update()
-    {
-        cooldownsUpdateMethods();
-    }
-
-    private void AssignCooldownToDelegate(Cooldown cooldown)
-    {
-        // delegate has no subscribers
-        if (cooldownsUpdateMethods == null)
+        private void Update()
         {
-            // assign the first subscriber
-            cooldownsUpdateMethods = cooldown.Update;
+            cooldownsUpdates();
         }
-        else
+
+        internal void AddToManager(Action call)
         {
-            // add an additional subscriber
-            cooldownsUpdateMethods += cooldown.Update;
+            // delegate has no subscribers
+            if (cooldownsUpdates == null)
+            {
+                // assign the first subscriber
+                cooldownsUpdates = call;
+            }
+            else
+            {
+                // add an additional subscriber
+                cooldownsUpdates += call;
+            }
         }
     }
-
-    #endregion
 }
