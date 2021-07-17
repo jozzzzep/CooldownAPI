d272dbd0c523f5acf1e8c659ccdcafec6396383d
JosepeDev <yosefd99@gmail.com>
2021-07-17 14:56:54 +0300

    Documented example 1

--- src/CooldownAPI/Assets/Examples/01/Gun.cs
+++ src/CooldownAPI/Assets/Examples/01/Gun.cs
@@ -5,28 +5,35 @@ using CooldownAPI;
 
 public class Gun : MonoBehaviour
 {
+    // initialize the cooldown
     Cooldown cd_shooting;
 
     void Start()
     {
+        // allocate the cooldown
         cd_shooting = new Cooldown(3f);
     }
 
     void Update()
     {
+        // check if cooldown is not active (player can shoot)
         if (cd_shooting.IsActive == false)
         {
+            // check if player pressed left mouse button
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
                 Shoot();
             }
         }
+
+        // debug the cooldown's timer
         Debug.Log("Cooldown timer: " + cd_shooting.Timer);
     }
 
     void Shoot()
     {
         Debug.Log("pew");
+        // activate cooldown
         cd_shooting.Activate();
     }
 }
